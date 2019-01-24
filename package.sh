5dd530fb0a5fa9128fe34653f590bf87218bb10c
Tomasz Cichocki <tomasz.cichocki@optiva.com>
2019-01-24 19:43:18 +0100

    Handle requirements

--- package.sh
+++ package.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-rm lambda.zip
-cd package
-zip -r9 ../lambda.zip .
-cd ..
-cp config-lambda.json config.json
-zip -g lambda.zip *.py config.json
-cp config-local.json config.json
+update_package() {
+  rm -rf package
+  pip install -r requirements.txt --target package
+}
+
+create_zip() {
+  rm lambda.zip
+  cd package
+  zip -r9 ../lambda.zip .
+  cd ..
+  cp config-lambda.json config.json
+  zip -g lambda.zip *.py config.json
+  cp config-local.json config.json
+}
+
+if [ $1 == "update" ]
+then update_package
+else create_zip
+fi
